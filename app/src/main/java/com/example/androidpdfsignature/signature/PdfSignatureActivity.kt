776e8f91cc8ece8b39a38c255c03dc0be185d2a2
Nam.Pham <wata.nam.pham@pharmapoc.com>
2021-03-15 13:53:21 +0700

    pdf form field

--- app/src/main/java/com/example/androidpdfsignature/signature/PdfSignatureActivity.kt
+++ app/src/main/java/com/example/androidpdfsignature/signature/PdfSignatureActivity.kt
@@ -13,9 +13,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnNextLayout
 import androidx.lifecycle.lifecycleScope
 import com.example.androidpdfsignature.R
+import com.tom_roush.pdfbox.cos.COSName
 import com.tom_roush.pdfbox.pdmodel.PDDocument
 import com.tom_roush.pdfbox.pdmodel.PDPage
 import com.tom_roush.pdfbox.pdmodel.PDPageContentStream
+import com.tom_roush.pdfbox.pdmodel.encryption.AccessPermission
+import com.tom_roush.pdfbox.pdmodel.encryption.ProtectionPolicy
+import com.tom_roush.pdfbox.pdmodel.encryption.StandardProtectionPolicy
 import com.tom_roush.pdfbox.pdmodel.graphics.image.LosslessFactory
 import com.tom_roush.pdfbox.rendering.PDFRenderer
 import kotlinx.android.synthetic.main.activity_pdf_signature.*
@@ -32,13 +36,10 @@ import java.io.OutputStream
 class PdfSignatureActivity : AppCompatActivity() {
 
     companion object {
-        private const val PDF_NAME = "sample.pdf"
+        private const val PDF_NAME = "sample5.pdf"
     }
 
     private var document: PDDocument? = null
-
-    private var page: PDPage? = null
-        get() = document?.getPage(currentPage)
 
     private var currentPage = 0
     private val displayMetrics = DisplayMetrics()
@@ -109,7 +110,7 @@ class PdfSignatureActivity : AppCompatActivity() {
                     // Create a renderer for the document
                     val renderer = PDFRenderer(document)
                     // Render the image to an RGB Bitmap
-                    renderer.renderImage(currentPage, 1f, Bitmap.Config.ARGB_8888)
+                    renderer.renderImage(currentPage, 1f, Bitmap.Config.RGB_565)
                 } catch (e: IOException) {
                     Log.e("PdfBox-Android-Sample", "Exception thrown while rendering file", e)
                     null
@@ -121,7 +122,10 @@ class PdfSignatureActivity : AppCompatActivity() {
 
             // Why doOnNextLayout() -> check doc
             image_choose_pdf?.doOnNextLayout {
-                image_choose_pdf.setUpMagnifinerBitmap(pageImage ?: return@doOnNextLayout, cropTrimBitmap)
+                image_choose_pdf.setUpMagnifinerBitmap(
+                    pageImage ?: return@doOnNextLayout,
+                    cropTrimBitmap
+                )
             }
             showLoading(false)
         }
@@ -137,6 +141,7 @@ class PdfSignatureActivity : AppCompatActivity() {
             .onPageError { _, _ ->
 
             }
+            .enableAnnotationRendering(true) // enable for filled form - color - comment
             .enableSwipe(false)
             // k đc dùng cái này mà phải define cụ thể spacing() -> xem doc
 //            .autoSpacing(true)
@@ -153,7 +158,7 @@ class PdfSignatureActivity : AppCompatActivity() {
 
     private suspend fun bitmapToFile(bitmap: Bitmap): String? = withContext(Dispatchers.IO) {
         delay(1000)
-        if (document == null || page == null) {
+        if (document == null) {
             null
         } else {
             // Get the context wrapper
@@ -165,61 +170,98 @@ class PdfSignatureActivity : AppCompatActivity() {
 
             val scaleByGesture = image_choose_pdf?.getSignatureImageScale() ?: 1f
 
-            try {
-                // Compress the bitmap and save in jpg format
-                val stream: OutputStream = FileOutputStream(file)
-                val pageSize = pdf_view.getPageSize(currentPage)
-                var scaleBitmap = Bitmap.createScaledBitmap(
-                    bitmap,
-                    (bitmap.width * (page!!.bBox.width / pageSize.width) * scaleByGesture).toInt(),
-                    (bitmap.height * (page!!.bBox.height / pageSize.height) * scaleByGesture).toInt(),
-                    false
-                )
-                scaleBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                stream.flush()
-                stream.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            file.absolutePath
-        }
-    }
-
-    private suspend fun fillForm(imagePath: String) = withContext(Dispatchers.IO) {
-        if (document == null || page == null) {
-
-        } else {
-            try {
-                var contentStream = PDPageContentStream(document, page, true, true)
-
-                // Draw the red overlay image
-                val pageSize = pdf_view.getPageSize(currentPage)
-
-                val insertImage = BitmapFactory.decodeFile(imagePath)
-                val insertXImage = LosslessFactory.createFromImage(document, insertImage)
-                val x = image_choose_pdf.signatureBitmapX * (page!!.bBox.width / pageSize.width)
-                val y =
-                    (page!!.bBox.height - (image_choose_pdf.signatureBitmapY * (page!!.bBox.height / pageSize.height)))
-
-                contentStream.drawImage(
-                    insertXImage,
-                    x,
-                    y - insertImage.height
-                )
-                contentStream.fill()
-                // Make sure that the content stream is closed:
-                contentStream.close()
-
-                val paths: String = applicationContext.cacheDir.absolutePath + "/FilledForm.pdf"
-
-                document!!.save(paths)
-                document!!.close()
-                document = null
-            } catch (ex: Exception) {
-                ex.printStackTrace()
+            val page = document?.getPage(currentPage)
+            if (page != null) {
+                try {
+                    // Compress the bitmap and save in jpg format
+                    val stream: OutputStream = FileOutputStream(file)
+                    val pageSize = pdf_view.getPageSize(currentPage)
+                    var scaleBitmap = Bitmap.createScaledBitmap(
+                        bitmap,
+                        (bitmap.width * (page.bBox.width / pageSize.width) * scaleByGesture).toInt(),
+                        (bitmap.height * (page.bBox.height / pageSize.height) * scaleByGesture).toInt(),
+                        false
+                    )
+                    scaleBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    stream.flush()
+                    stream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                file.absolutePath
+            } else {
+                null
             }
         }
     }
+
+    private suspend fun fillForm(
+        imagePath: String,
+        isEncryptPdf: Boolean = false,
+        isFormFieldReadOnly: Boolean = false
+    ) =
+        withContext(Dispatchers.IO) {
+            if (document == null) {
+
+            } else {
+                val page = document?.getPage(currentPage)
+                if (page != null) {
+                    try {
+                        var contentStream = PDPageContentStream(document, page, true, true)
+
+                        // Draw the red overlay image
+                        val pageSize = pdf_view.getPageSize(currentPage)
+
+                        val insertImage = BitmapFactory.decodeFile(imagePath)
+                        val insertXImage = LosslessFactory.createFromImage(document, insertImage)
+                        val x =
+                            image_choose_pdf.signatureBitmapX * (page.bBox.width / pageSize.width)
+                        val y =
+                            (page.bBox.height - (image_choose_pdf.signatureBitmapY * (page.bBox.height / pageSize.height)))
+
+                        contentStream.drawImage(
+                            insertXImage,
+                            x,
+                            y - insertImage.height
+                        )
+                        contentStream.fill()
+                        // Make sure that the content stream is closed:
+                        contentStream.close()
+
+                        val paths: String =
+                            applicationContext.cacheDir.absolutePath + "/FilledForm.pdf"
+
+                        if (isFormFieldReadOnly) {
+                            document!!.documentCatalog.acroForm?.fields?.let {
+                                it.forEach { s ->
+                                    s.isReadOnly = true
+                                }
+                            }
+                        }
+
+                        if (isEncryptPdf) {
+                            document?.protect(
+                                StandardProtectionPolicy(
+                                    "ownerpass",
+                                    "userpass",
+                                    AccessPermission().apply {
+                                        setCanModify(false)
+                                        setCanFillInForm(false)
+                                        setReadOnly()
+                                        setCanModifyAnnotations(false)
+                                        setCanExtractForAccessibility(false)
+                                    })
+                            )
+                        }
+                        document!!.save(paths)
+                        document!!.close()
+                        document = null
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
+                }
+            }
+        }
 
     private fun showLoading(show: Boolean = true) {
         view_loading.visibility = if (show) View.VISIBLE else View.GONE
